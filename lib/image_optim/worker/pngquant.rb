fb20d233e75beca06f6e222760af981f1aa4f14a
ilya shcherbinin <m0n9oose@gmail.com>
2019-08-12 16:02:50 +0200

    Add strip_tags option
    to pngquant, oxipng, jpegtran workers

--- lib/image_optim/worker/pngquant.rb
+++ lib/image_optim/worker/pngquant.rb
@@ -8,9 +8,11 @@ class ImageOptim
   class Worker
     # http://pngquant.org/
     class Pngquant < Worker
-
-      IEDEBUG =
+      IEBUG =
       option(:iebug, false, 'iebug'){ |v| !!v }
+
+      STRIP_TAGS =
+      option(:strip_tags, false, 'Remove optional metadata') { |v| !!v }
 
       ALLOW_LOSSY_OPTION =
       option(:allow_lossy, false, 'Allow quality option'){ |v| !!v }
@@ -62,10 +64,11 @@ class ImageOptim
           --skip-if-larger
           --force
         ]
+        args.push("--strip") if strip_tags
         args.push("--iebug") if iebug
-        args.push("#{max_colors}")
+        args.push(max_colors.to_s)
         args.push("--")
-        args.push("#{src}")
+        args.push(src.to_s)
         execute(:pngquant, args, options) && optimized?(src, dst)
       end
     end
