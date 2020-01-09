22fab17d56c1d2d761a5ae4be5240b8932e37ed4
Ilya Shcherbinin <m0n9oose@gmail.com>
2020-01-09 15:22:39 +0100

    Update jpeg-recompress worker

--- lib/image_optim/worker/jpegrecompress.rb
+++ lib/image_optim/worker/jpegrecompress.rb
@@ -5,23 +5,21 @@ class ImageOptim
   class Worker
     # https://github.com/danielgtaylor/jpeg-archive#jpeg-recompress
     class Jpegrecompress < Worker
-      ALLOW_LOSSY_OPTION =
-      option(:allow_lossy, false, 'Allow worker, it is always lossy'){ |v| !!v }
+      ALLOW_LOSSY_OPTION = option(:allow_lossy, false, 'Allow worker, it is always lossy') { |v| !!v }
+      STRIP_TAGS = option(:strip_tags, false, 'Remove optional metadata') { |v| !!v }
+      METHODS = %w[mpe ssim ms-ssim smallfry].freeze
+      LOOPS = option(:loops, 6, 'Set the number of runs to attempt') { |v| OptionHelpers.limit_with_range(v.to_i, 1..16) }
+      QUALITY_MIN = option(:quality_min, 50, 'Minimum JPEG quality') { |v| OptionHelpers.limit_with_range(v.to_i, 30..70) }
+      QUALITY_MAX = option(:quality_max, 90, 'Maximum JPEG quality') { |v| OptionHelpers.limit_with_range(v.to_i, 70..100) }
+      COMPRESSION_METHOD = option(:compression_method, 'smallfry', 'Set comparison method') do |v|
+        selected = v.to_s
+        METHODS.include?(selected) ? selected : 'smallfry'
+      end
+      QUALITY_NAMES = %i[low medium high veryhigh].freeze
 
       # Initialize only if allow_lossy
       def self.init(image_optim, options = {})
         super if options[:allow_lossy]
-      end
-
-      QUALITY_NAMES = [:low, :medium, :high, :veryhigh].freeze
-
-      quality_names_desc = QUALITY_NAMES.each_with_index.map do |name, i|
-        "`#{i}` - #{name}"
-      end.join(', ')
-
-      QUALITY_OPTION =
-      option(:quality, 3, "JPEG quality preset: #{quality_names_desc}") do |v|
-        OptionHelpers.limit_with_range(v.to_i, 0...QUALITY_NAMES.length)
       end
 
       def used_bins
@@ -35,11 +33,18 @@ class ImageOptim
 
       def optimize(src, dst)
         args = %W[
-          --quality #{QUALITY_NAMES[quality]}
+          --min #{ quality_min }
+          --max #{ quality_max }
           --no-copy
-          #{src}
-          #{dst}
+          --method #{ compression_method }
+          --loops #{ loops }
+          --accurate
         ]
+
+        args.push('--strip') if strip_tags
+        args.push(src)
+        args.push(dst)
+
         execute(:'jpeg-recompress', *args) && optimized?(src, dst)
       end
     end
